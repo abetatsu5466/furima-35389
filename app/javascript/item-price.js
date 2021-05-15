0c568896576c98ed5f4077bcd680b4df8f8a464b
abetatsu5466 <82029454+abetatsu5466@users.noreply.github.com>
2021-05-15 15:23:41 +0900

    JS金額計算

---
+++ app/javascript/item-price.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', () => {
+ const priceInput = document.getElementById("item-price");
+   priceInput.addEventListener("input", () => {
+　　const inputValue = priceInput.value;
+
+
+    const tax = inputValue * 0.1
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(tax)
+
+
+    const profitDom = document.getElementById("profit");
+    profitDom.innerHTML = tax
+    profitDom.innerHTML = 　Math.floor(inputValue - tax)
+
+})
+
+});
+
