fe820762c1e8b0a129c6266dcce936bee1268754
abetatsu5466 <82029454+abetatsu5466@users.noreply.github.com>
2021-05-17 17:57:39 +0900

    修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,only: [:new, :create,  :edit]
-  before_action :set_item, only: [:edit, :show]
+  before_action :set_item, only: [:show]
 
 
   def index
