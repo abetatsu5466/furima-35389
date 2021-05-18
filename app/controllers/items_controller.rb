cadc079655b4e6bb938604dea1ad843ca47fe94c
abetatsu5466 <82029454+abetatsu5466@users.noreply.github.com>
2021-05-18 19:00:49 +0900

    削除成功

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,only: [:new, :create, :edit, :update]
-  before_action :set_item, only: [:edit, :update, :show]
+  before_action :authenticate_user!,only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_item, only: [:edit, :update, :show,]
   before_action :edit_up, only: [ :edit, :update]
 
   def index
@@ -32,6 +32,12 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
   end
 
   private
